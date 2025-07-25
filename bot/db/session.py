0518b11eaf99c2b93512aaf1903899c1cafa9af4
AlexeevID <46222556+alexeevid@users.noreply.github.com>
2025-07-25 22:52:15 +0300

    Update session.py

--- bot/db/session.py
+++ bot/db/session.py
@@ -7,6 +7,6 @@ DB_URL = settings.database_url
 engine = create_engine(DB_URL, echo=True, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-def init_db():
+def init_db(Base):
     from . import models
     models.Base.metadata.create_all(bind=engine)
