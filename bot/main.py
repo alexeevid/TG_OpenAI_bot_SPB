42b90054cb2b4a77335ad8a772debde300951f74
AlexeevID <46222556+alexeevid@users.noreply.github.com>
2025-07-26 01:00:16 +0300

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,20 +1,4 @@
-import asyncio
-from telegram.ext import ApplicationBuilder
-from bot.telegram_bot import ChatGPTTelegramBot
-from bot.settings import settings
-
-
-async def main():
-    app = ApplicationBuilder().token(settings.telegram_bot_token).build()
-    bot = ChatGPTTelegramBot(settings)
-
-    await bot.initialize(app)
-    await app.initialize()
-    await app.start()
-    await app.updater.start_polling()
-    await app.updater.idle()
-
+from bot.telegram_bot import app
 
 if __name__ == "__main__":
     app.run_polling()
-
