1f083504ad1356e8ad0acffa0736e85862132af6
AlexeevID <46222556+alexeevid@users.noreply.github.com>
2025-07-31 17:06:52 +0300

    Update __init__.py

--- bot/knowledge_base/__init__.py
+++ bot/knowledge_base/__init__.py
@@ -1,12 +1,2 @@
 # bot/knowledge_base/__init__.py
-
-from .indexer import KBDocument, KnowledgeBaseIndexer
-from .retriever import KnowledgeBaseRetriever
-from .context_manager import ContextManager
-
-__all__ = [
-    "KBDocument",
-    "KnowledgeBaseIndexer",
-    "KnowledgeBaseRetriever",
-    "ContextManager",
-]
+# Пакет Базы знаний
