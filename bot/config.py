45542afe7f5b4c245dc8892ee7dafd9fa643e255
AlexeevID <46222556+alexeevid@users.noreply.github.com>
2025-07-31 17:08:45 +0300

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,4 +1,7 @@
+# bot/config.py
+from __future__ import annotations
 from bot.settings import Settings
 
+
 def load_settings() -> Settings:
-    return Settings()
+    return Settings()  # Pydantic Settings сам подтянет переменные окружения
