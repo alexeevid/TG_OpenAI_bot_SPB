8c95e115445b14a4667d0aa7df90f68b73b07247
AlexeevID <46222556+alexeevid@users.noreply.github.com>
2025-08-05 20:59:43 +0300

    Update session.py

--- bot/db/session.py
+++ bot/db/session.py
@@ -1,16 +1,13 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-from bot.settings import settings
 
-DB_URL = settings.database_url
+DB_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DB_URL, echo=True, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-# Объявление Base здесь
 Base = declarative_base()
 
 def init_db():
-    # импорт моделей внутри функции, чтобы избежать циклического импорта
     from . import models
     Base.metadata.create_all(bind=engine)
