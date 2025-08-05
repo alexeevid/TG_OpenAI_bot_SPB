7aafff5cad4b0041cb8218ec1ae11c678fffe5bd
AlexeevID <46222556+alexeevid@users.noreply.github.com>
2025-08-05 21:17:18 +0300

    Update session.py

--- bot/db/session.py
+++ bot/db/session.py
@@ -2,7 +2,11 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DB_URL = os.getenv("DATABASE_URL")
+DB_URL = os.getenv("DATABASE_URL", "")
+
+# Авто-замена для совместимости с SQLAlchemy
+if DB_URL.startswith("postgres://"):
+    DB_URL = DB_URL.replace("postgres://", "postgresql://", 1)
 
 engine = create_engine(DB_URL, echo=True, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
