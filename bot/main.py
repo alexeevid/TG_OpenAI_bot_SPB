9a3b819a6117081e1aae43c1d278af3ea93e7a68
AlexeevID <46222556+alexeevid@users.noreply.github.com>
2025-08-12 08:53:58 +0300

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,22 +1,11 @@
-import logging
-from bot.settings import load_settings
+# bot/main.py
 from bot.telegram_bot import build_app
+from telegram import Update
 
 def main():
-    settings = load_settings()
-    # ✅ более «шумная» настройка логов
-    logging.basicConfig(
-        level=getattr(logging, settings.log_level.upper(), logging.INFO),
-        format="%(asctime)s %(levelname)s [%(name)s] %(message)s",
-        force=True,
-    )
-    # немного настроим сторонние логгеры
-    logging.getLogger("telegram").setLevel(logging.INFO)    # чтобы не заливало дебагом PTB
-    logging.getLogger("sqlalchemy.engine").setLevel(logging.INFO)  # видеть SQL (можно DEBUG)
-    logging.getLogger("bot").setLevel(logging.DEBUG)        # наши модули
-
     app = build_app()
-    app.run_polling(allowed_updates=None)
+    # run_polling сам создаёт/закрывает цикл, post_init выполнится перед стартом
+    app.run_polling(drop_pending_updates=True, allowed_updates=Update.ALL_TYPES)
 
-if __name__=='__main__':
+if __name__ == "__main__":
     main()
