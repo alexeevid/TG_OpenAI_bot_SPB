f27da7a7239270ec4c07eebc3047778ca1a3a9fc
AlexeevID <46222556+alexeevid@users.noreply.github.com>
2025-10-05 22:53:48 +0300

    Update image_service.py

--- app/services/image_service.py
+++ app/services/image_service.py
@@ -1,7 +1,14 @@
+from openai import OpenAI
 
 class ImageService:
-    def __init__(self, openai_client, model: str):
-        self._cli = openai_client
-        self._model = model
-    def generate(self, prompt: str) -> str:
-        return self._cli.image(prompt, self._model)
+    def __init__(self, api_key: str, image_model: str = "gpt-image-1"):
+        self.client = OpenAI(api_key=api_key)
+        self.image_model = image_model
+
+    def generate(self, prompt: str, size: str = "1024x1024") -> str:
+        res = self.client.images.generate(
+            model=self.image_model,
+            prompt=prompt,
+            size=size,
+        )
+        return res.data[0].url
