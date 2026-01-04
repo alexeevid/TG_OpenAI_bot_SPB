2c04511c95a0f72c95b410476607ecf83d47bb50
AlexeevID <46222556+alexeevid@users.noreply.github.com>
2026-01-04 23:53:14 +0300

    Delete kb directory

--- kb/embedder.py
+++
@@ -1,7 +0,0 @@
-
-class Embedder:
-    def __init__(self, openai_client, model: str):
-        self._cli = openai_client
-        self._model = model
-    def embed(self, texts: list[str]) -> list[list[float]]:
-        return self._cli.embeddings(texts, self._model)
